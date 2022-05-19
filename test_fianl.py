e482b716dffe557b6ce0d5bfe9981c2e2b71d236
Isaac Telloian <iketelloian@fresnostate.edu>
2022-05-19 15:53:14 -0700

    finished structured-english

---
+++ test_fianl.py
@@ -0,0 +1,16 @@
+# Laymen's Terms
+"""
+The program displays three main functions;
+the number of grades, the class average, and the % of grades > average.
+
+using the "Final.txt" file we;
+first open print the total number of grades = "num_grades" found on the file
+add up the sum of all "grades" found in file
+by dividing the sum of "grades" by the "num_grades" we get "class_avg"
+print class average "class_avg" to user
+using the "class_avg" the program can then output the % of grades that are > average
+
+the functions will output the number of grades
+then loop through the scores and output a class average
+and finaly, loop and output the % of all grades that are > average
+"""
